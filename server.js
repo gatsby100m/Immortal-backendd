f39b0912e32356fd0e6f018328c05a5ca54c6aa0
gatsby100m <gritoujoshua66@gmail.com>
2025-06-02 16:28:19 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/movies", require("./routes/movies"));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => console.log("Server running on port", process.env.PORT));
+  })
+  .catch(err => console.error(err));
