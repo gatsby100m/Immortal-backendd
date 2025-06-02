48d63f12a546692c04a4a3320710c5d57febe451
gatsby100m <gritoujoshua66@gmail.com>
2025-06-02 16:56:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+// server.js
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 const app = express();
@@ -14,8 +15,9 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/movies", require("./routes/movies"));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => console.log("Server running on port", process.env.PORT));
-  })
-  .catch(err => console.error(err));
+// Connect to DB and Start Server
+connectDB().then(() => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+  );
+});
